519d9bf271bffb1c670e3ceb5912534927a572ef
Yudi Wiradinata <yudiqs@gmail.com>
2017-12-21 17:00:18 +0700

    initial project helloworld

---
+++ src/main/java/com/yudi/SpringAwsStripeApplication.java
@@ -0,0 +1,12 @@
+package com.yudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsStripeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yudi/SpringAwsStripeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yudi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAwsStripeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
