932a91aac7c428ac40c7b295429a6263e1723425
Yudi Wiradinata <yudiqs@gmail.com>
2017-12-26 13:22:57 +0700

    add logging

--- src/main/java/com/yudi/web/i18n/I18NService.java
+++ src/main/java/com/yudi/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.yudi.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -13,10 +15,14 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /*the application logger*/
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId) {
+        LOG.info("GetMessage by Id: {}",messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId,locale);
     }
