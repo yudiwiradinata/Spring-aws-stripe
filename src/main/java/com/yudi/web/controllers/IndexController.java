8e6c6272dc21dd7cf11aa51e06c025ba431b5cad
Yudi Wiradinata <yudiqs@gmail.com>
2017-12-26 10:21:27 +0700

    add index.html

--- src/main/java/com/yudi/web/controllers/IndexController.java
+++ src/main/java/com/yudi/web/controllers/IndexController.java
@@ -1,7 +1,16 @@
 package com.yudi.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Yudi on 22/12/2017.
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
 }
